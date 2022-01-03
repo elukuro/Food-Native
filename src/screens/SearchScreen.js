6bb22fcb6e944cbb26735862adc216bc80dcab1a
elukuro <heruhartantot@gmail.com>
2022-01-03 08:29:56 +0700

    section 9 102

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,21 +6,25 @@ import yelp from "./../api/yelp";
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const searchAPI = async () => {
+  const searchAPI = async (searchTerm) => {
     try {
       const response = await yelp.get("/search", {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: "san jose",
         },
       });
       setResults(response.data.businesses);
+      setErrorMessage("");
     } catch (err) {
-      console.log(err);
+      setErrorMessage("Something went wrong");
     }
   };
+
+  // caled searchAPI imediately after screenr rendered are BAD CODE
 
   return (
     <View>
@@ -30,12 +34,12 @@ const SearchScreen = () => {
           setTerm(newTerm);
         }}
         onTermSubmit={() => {
-          searchAPI();
+          searchAPI(term);
         }}
         // simplify by
         // onTermSubmit={searchAPI}
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
